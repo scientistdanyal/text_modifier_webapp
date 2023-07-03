ea1c4c256d3a291cd86eccbcd5e88790eb17f1e6
Danyal Ahmed <dk.yalgar@gmail.com>
2023-07-03 02:45:31 -0700

    changes are made

--- myfirst_web_app/myfirst_web_app/views.py
+++ myfirst_web_app/myfirst_web_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("hello world")
+    return HttpResponse('''<h1>Hello world </h1><br> <a href='https://www.google.com/'>Click me</a>''')
 
 def about(request):
     return HttpResponse("about danyal")
