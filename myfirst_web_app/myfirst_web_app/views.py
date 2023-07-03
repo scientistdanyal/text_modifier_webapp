30a38f42cb806910cade6dc7da0928a61a643409
Danyal Ahmed <dk.yalgar@gmail.com>
2023-07-03 03:05:08 -0700

    back button is added to about page

--- myfirst_web_app/myfirst_web_app/views.py
+++ myfirst_web_app/myfirst_web_app/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse('''<h1>Hello world </h1><br> <a href='https://www.google.com/'>Click me</a>''')
 
 def about(request):
-    return HttpResponse("about danyal")
+    return HttpResponse('''<h1>about danyal</h1><br><a href='http://127.0.0.1:8000/'>back button</a>''')
