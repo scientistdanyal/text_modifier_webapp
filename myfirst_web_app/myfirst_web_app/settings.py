8015a03fe00e02086c0acbbb867e63793268bcd3
Danyal Ahmed <dk.yalgar@gmail.com>
2023-07-04 11:35:02 -0700

    allowed host all

--- myfirst_web_app/myfirst_web_app/settings.py
+++ myfirst_web_app/myfirst_web_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nonl@(lib)fux3kos^z$8_ys&_@ka^&bh-_b*=bkb&-lk@n6%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
