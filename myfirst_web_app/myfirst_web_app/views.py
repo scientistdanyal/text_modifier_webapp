c5092852453544bbea92db331164b620ed816def
Danyal Ahmed <dk.yalgar@gmail.com>
2023-07-04 02:36:51 -0700

    capitalization and remove new line func are added

--- myfirst_web_app/myfirst_web_app/views.py
+++ myfirst_web_app/myfirst_web_app/views.py
@@ -12,16 +12,34 @@ def analyse(request):
    #get the text
    djtext = request.GET.get('text','default')
    removepunc = request.GET.get('removepunc','off')
-#    print(removepunc)
-#    print(djtext)
-   punctutaions ='''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-   anaylzed = ""
-   for char in djtext:
-       if char not in punctutaions:
-           anaylzed = anaylzed + char
+   fullcaps = request.GET.get('fullcaps','off')
+   newlineremover = request.GET.get('removeNewLine','off')
 
-   prams = {"purpose":"Remove Punction","anlyzed":anaylzed}       
-   return render(request, 'removepunc.html',prams)
+   # removal of punctuation
+   if removepunc == 'on':
+    punctutaions ='''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    anaylzed = ""
+    for char in djtext:
+        if char not in punctutaions:
+            anaylzed = anaylzed + char
+    prams = {"purpose":"Remove Punctuation","anlyzed":anaylzed}       
+    return render(request, 'removepunc.html',prams)
+   
+   # capitalization of text
+   elif fullcaps == 'on':
+    anaylzed = djtext.upper()
+    prams = {"purpose":"Convert to Uppercase","anlyzed":anaylzed}
+    return render(request, 'removepunc.html',prams)
+   
+   # new line remover
+   elif newlineremover == 'on':
+    anaylzed = ""
+    for char in djtext:
+       if char != '\n':
+          anaylzed = anaylzed + char
+    
+    prams = {"purpose":"New Lines are Removed","anlyzed":anaylzed}
+    return render(request, 'removepunc.html',prams)
 
 # def capitalize(request):
 #     return HttpResponse('''<h1>about danyal</h1><br>
