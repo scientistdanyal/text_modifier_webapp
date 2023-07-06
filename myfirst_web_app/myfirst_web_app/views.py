32b51547f0e5353e606a68417106b925fca66a29
Danyal Ahmed <dk.yalgar@gmail.com>
2023-07-06 10:41:21 -0700

    csrf_token and post are added to form

--- myfirst_web_app/myfirst_web_app/views.py
+++ myfirst_web_app/myfirst_web_app/views.py
@@ -1,52 +1,60 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
 
 def analyse(request):
-    djtext = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepunc', 'off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('removeNewLine', 'off')
-    extraspacesremover = request.GET.get('extraspacesremover', 'off')
-    charcount = request.GET.get('charcount', 'off')
+    djtext = request.POST.get('text', 'default')
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('removeNewLine', 'off')
+    extraspacesremover = request.POST.get('extraspacesremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
+    if removepunc == 'on' or newlineremover == 'on' or charcount == 'on' or extraspacesremover == 'on' :
+        if removepunc == 'on':
+            punctuations = '''!()-[]};{:'"\,<>./?@#$%^&*_~'''
+            analyzed = ""
+            for char in djtext:
+                if char not in punctuations:
+                    analyzed += char
+            prams = {"purpose": "Remove Punctuation", "analyzed": analyzed}
+        djtext = analyzed    
+            # return render(request, 'analyze.html', prams)
 
-    if removepunc == 'on':
-        punctuations = '''!()-[]};{:'"\,<>./?@#$%^&*_~'''
-        analyzed = ""
-        for char in djtext:
-            if char not in punctuations:
+        if fullcaps == 'on':
+            analyzed = djtext.upper()
+            prams = {"purpose": "Convert to Uppercase", "analyzed": analyzed}
+        djtext = analyzed
+            # return render(request, 'analyze.html', prams)
+
+        if newlineremover == 'on':
+            analyzed = ""
+            for char in djtext:
+                if char != '\n':
+                    analyzed += char
+            prams = {"purpose": "New Lines are Removed", "analyzed": analyzed}
+        djtext = analyzed
+            # return render(request, 'analyze.html', prams)
+
+        if extraspacesremover == 'on':
+            analyzed = ""
+            for index, char in enumerate(djtext):
+                if djtext[index] == ' ' and djtext[index - 1] == ' ':
+                    pass
                 analyzed += char
-        prams = {"purpose": "Remove Punctuation", "analyzed": analyzed}
-        return render(request, 'analyze.html', prams)
+            prams = {"purpose": "Extra Spaces are Removed", "analyzed": analyzed}
+        djtext = analyzed
+            # return render(request, 'analyze.html', prams)
 
-    elif fullcaps == 'on':
-        analyzed = djtext.upper()
-        prams = {"purpose": "Convert to Uppercase", "analyzed": analyzed}
-        return render(request, 'analyze.html', prams)
 
-    elif newlineremover == 'on':
-        analyzed = ""
-        for char in djtext:
-            if char != '\n':
-                analyzed += char
-        prams = {"purpose": "New Lines are Removed", "analyzed": analyzed}
-        return render(request, 'analyze.html', prams)
+        if charcount == 'on':
+            analyzed = len(djtext)
+            prams = {"purpose": "Count Characters", "analyzed": analyzed}
+        djtext = analyzed
+            # return render(request, 'analyze.html', prams)
 
-    elif extraspacesremover == 'on':
-        analyzed = ""
-        for index, char in enumerate(djtext):
-            if djtext[index] == ' ' and djtext[index - 1] == ' ':
-                pass
-            analyzed += char
-        prams = {"purpose": "Extra Spaces are Removed", "analyzed": analyzed}
         return render(request, 'analyze.html', prams)
-
-    elif charcount == 'on':
-        analyzed = len(djtext)
-        prams = {"purpose": "Count Characters", "analyzed": analyzed}
-        return render(request, 'analyze.html', prams)
-
     else:
         return HttpResponse('error')
